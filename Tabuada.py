70931dec2a86b88015e6cad7392df8f453aab517
Cristina Ribeiro <53585855+CristicRibeiro@users.noreply.github.com>
2020-04-01 11:20:44 -0300

    Create Tabuada.py

---
+++ Tabuada.py
@@ -0,0 +1,14 @@
+
+
+
+num = int(input('digite um numero inteiro: '))
+print('{} x 01 = {}'.format(num, (num*1)))
+print('{} x 02 = {}'.format(num, (num*2)))
+print('{} x 03 = {}'.format(num, (num*2)))
+print('{} x 04 = {}'.format(num, (num*4)))
+print('{} x 05 = {}'.format(num, (num*5)))
+print('{} x 06 = {}'.format(num, (num*6)))
+print('{} x 07 = {}'.format(num, (num*7)))
+print('{} x 08 = {}'.format(num, (num*8)))
+print('{} x 09 = {}'.format(num, (num*9)))
+print('{} x 10 = {}'.format(num, (num*10)))
