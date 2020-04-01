7cd9de6637553a939ad687dd5eda9e5c77263357
Cristina Ribeiro <53585855+CristicRibeiro@users.noreply.github.com>
2020-04-01 11:30:24 -0300

    Create Format2.py

---
+++ Format2.py
@@ -0,0 +1,9 @@
+  
+'''
+Autor:Cristina Ribeiro
+Codigo: .format e conta de dividir (como formatar strinf e printar na tela)
+'''
+
+dinheiro = float(input('quanto dinheiro vc tem?? '))
+dolar = 3.27
+print('vc pode comprar {:.2f} dolares'.format(dinheiro/dolar))
