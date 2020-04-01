8ecd8182e55841665882d68330abba38639dae3a
Cristina Ribeiro <53585855+CristicRibeiro@users.noreply.github.com>
2020-04-01 11:27:44 -0300

    Create Concatenando.py

---
+++ Concatenando.py
@@ -0,0 +1,10 @@
+'''
+Autor:Cristina Ribeiro
+Codigo: Concatenando String (juntando string)
+'''
+
+print ('qual a sua data de nascimento?')
+dia = input ('digite o dia: ')
+mes = input ('digite o mes: ')
+ano = input ('digite o ano: ')
+print ('voce nasceu no dia ' +dia +' de ' +mes +' de ' +ano)
