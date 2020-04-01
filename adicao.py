b8cb30093fce5d790f3781f91d13ac5b5a239357
Cristina Ribeiro <53585855+CristicRibeiro@users.noreply.github.com>
2020-04-01 11:26:38 -0300

    Create adicao.py

---
+++ adicao.py
@@ -0,0 +1,10 @@
+'''
+Autor:Cristina Ribeiro
+Codigo: adicao usando python
+'''
+
+
+print ('digite dois numeros para a soma')
+n1 = input ('digite o primeiro numero: ')
+n2 = input ('digite o segundo numero: ')
+print (' a soma eh: ' + str (int(n1)+ int(n2)))
