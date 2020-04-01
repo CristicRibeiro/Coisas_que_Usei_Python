da75272aa1049ec47fce8946537b4bfc83e8dbcf
Cristina Ribeiro <53585855+CristicRibeiro@users.noreply.github.com>
2020-04-01 11:25:46 -0300

    Create Input.py

---
+++ Input.py
@@ -0,0 +1,12 @@
+'''
+Autor: Cristina Ribeiro
+Codigo: como funciona metodo input (mostrar na tela)
+'''
+
+
+
+nome = input ('qual eh o seu nome?')
+idade = input ('qual eh a sua idade?')
+peso = input ('qual eh o seu peso?')
+
+print (nome, idade, peso)
