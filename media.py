03edaf910f45cf71df2f60369c1eb09359b2301e
Cristina Ribeiro <53585855+CristicRibeiro@users.noreply.github.com>
2020-04-01 11:24:23 -0300

    Create media.py

---
+++ media.py
@@ -0,0 +1,10 @@
+  
+'''
+Autor:Cristina Ribeiro
+Codigo: add porcentagem - salario
+'''
+
+n1 = float(input('digite a nota 1 do aluno: '))
+n2 = float(input('digite a nota 2 do aluno: '))
+media = (n1+n2)/2
+print('a media do aluno eh {:.2f}'.format(media))
