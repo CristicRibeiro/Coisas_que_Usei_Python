b9b2b3546f25fa6ac9db8e6a0433a65cc8a8d602
Cristina Ribeiro <53585855+CristicRibeiro@users.noreply.github.com>
2020-04-01 11:23:12 -0300

    Create SalarioPorcent.py

---
+++ SalarioPorcent.py
@@ -0,0 +1,8 @@
+'''
+Autor:Cristina Ribeiro
+Codigo: add porcentagem - salario
+'''
+
+salario = float(input('digite o salario atual: '))
+aumento = 15/100
+print('o valor do salario com aumento sera:{:.2f}'.format(salario+(salario*aumento)))
